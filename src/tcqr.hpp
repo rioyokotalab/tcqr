149862f8d26735ad021285fc8d2cae961c6df3f8
mutsuki <mutsuki@momo86.net>
2018-12-23 20:32:03 +0900

    update : ノルム計算型とTCスイッチを追加

--- src/tcqr.hpp
+++ src/tcqr.hpp
@@ -2,8 +2,8 @@
 #define __TCQR_HPP__
 
 namespace tcqr{
-template <class Input_t, class Output_t>
-void qr16x16tc(Output_t* const q, Output_t* const r, const Input_t* const a, const std::size_t m, const std::size_t n);
+template <class Input_t, class Output_t, class Norm_t, bool UseTC>
+void qr16x16(Output_t* const q, Output_t* const r, const Input_t* const a, const std::size_t m, const std::size_t n);
 }
 
 #endif /* end of include guard */
