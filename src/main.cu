9a912d6be8798140b294f6164885da7cb9a91a63
mutsuki <mutsuki@momo86.net>
2019-01-06 18:16:55 +0900

    update : 行列サイズを16x16に戻した

--- src/main.cu
+++ src/main.cu
@@ -6,8 +6,8 @@
 #include "utils.hpp"
 #include "test.hpp"
 
-constexpr std::size_t M = 5;
-constexpr std::size_t N = 5;
+constexpr std::size_t M = 16;
+constexpr std::size_t N = 16;
 constexpr float rand_range = 1.0f;
 
 int main(int argc, char** argv){
@@ -26,20 +26,20 @@ int main(int argc, char** argv){
 	auto h_matrix_a = cutf::cuda::memory::get_host_unique_ptr<float>(M * N);
 	std::mt19937 mt(std::random_device{}());
 	std::uniform_real_distribution<float> dist(-rand_range, rand_range);
-	float mm[] = {16, 2, 1, 4, 1, -1, 12, 3, -2, 2, 1, 1, -24, 1, 3, 2, -1, 2, 20, -3, 1, 1, 1, 2, -20};
 	for(std::size_t i = 0; i < M * N; i++){
-		h_matrix_a.get()[i] = mm[i];
-		//h_matrix_a.get()[i] = dist(mt);
+		h_matrix_a.get()[i] = dist(mt) * (i % (M + 1) == 0 ? 30.0f : 1.0f);
 	}
 	
 	/*test::qr<float, float, float, true>(M, N, h_matrix_a.get());
+	test::qr<float, float, float, false>(M, N, h_matrix_a.get());
 	test::qr<half, half, half, true>(M, N, h_matrix_a.get());
 	test::qr<half, half, half, false>(M, N, h_matrix_a.get());
 	test::qr<half, half, float, true>(M, N, h_matrix_a.get());
 	test::qr<half, half, float, false>(M, N, h_matrix_a.get());*/
 
-	test::qr<float, float, float, false>(M, N, h_matrix_a.get());
 	test::eigen<float, float, false>(M, h_matrix_a.get());
-	//test::eigen<half, half, false>(M, h_matrix_a.get());
-	//test::eigen<half, half, true>(M, h_matrix_a.get());
+	test::eigen<half, half, false>(M, h_matrix_a.get());
+	test::eigen<half, half, true>(M, h_matrix_a.get());
+	test::eigen<half, float, false>(M, h_matrix_a.get());
+	test::eigen<half, float, true>(M, h_matrix_a.get());
 }
