5b050d6184b3b3a0c67b079ef38621b28a239a92
mutsuki <mutsuki@momo86.net>
2019-01-08 23:24:42 +0900

    update : batched qrのプロトタイプ宣言を追加

--- src/tcqr.hpp
+++ src/tcqr.hpp
@@ -4,6 +4,8 @@
 namespace tcqr{
 template <class Input_t, class Output_t, class Norm_t, bool UseTC>
 void qr16x16(Output_t* const q, Output_t* const r, const Input_t* const a, const std::size_t m, const std::size_t n);
+template <class Input_t, class Output_t, class Norm_t, bool UseTC>
+void qr16x16_batched(Output_t* const* const q, Output_t* const * const r, const Input_t* const* const a, const std::size_t m, const std::size_t n, const std::size_t batch_size);
 template <class T, class Norm_t, bool UseTC>
 void eigen16x16(T* const eigenvalues, const T* const a, std::size_t n);
 }
