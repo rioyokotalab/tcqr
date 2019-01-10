91c1b442e60e4e8f0201937d889b3a3bdb15e611
mutsuki <mutsuki@momo86.net>
2019-01-10 15:15:28 +0900

    update : 実数固有値行列かを判定する関数を追加

--- src/eigenqr.cpp
+++ src/eigenqr.cpp
@@ -28,4 +28,21 @@ void eigen16x16(float* const eigenvalues, const float* const a, std::size_t n){
 	}
 }
 
+bool is_real(const float* const a, std::size_t n){
+	Eigen::MatrixXf ma;
+	ma.resize(n, n);
+	for(std::size_t i = 0; i < n; i++){
+		for(std::size_t j = 0; j < n; j++){
+			ma(i, j) = a[i + j * n];
+		}
+	}
+	Eigen::EigenSolver<Eigen::MatrixXf> eigensolver(ma);
+	//std::cout<<<<std::endl;
+	float sum = 0.0f;
+	for(std::size_t i = 0; i < n; i++){
+		sum += std::abs(eigensolver.eigenvalues()[i].imag());
+	}
+	return (sum == 0.0f);
+}
+
 }
--- src/eigenqr.hpp
+++ src/eigenqr.hpp
@@ -2,6 +2,7 @@
 #define __EIGENQR_HPP__
 namespace eigenqr{
 void eigen16x16(float* const eigenvalues, const float* const a, const std::size_t n);
+bool is_real(const float* const a, const std::size_t n);
 }
 
 #endif /* end of include guard */
